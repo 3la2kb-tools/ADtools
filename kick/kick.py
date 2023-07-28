c3ad918566b910dc796e75266ae04eb145c48679
3la2kb-tools <60458175+3la2kb-tools@users.noreply.github.com>
2023-07-28 10:36:17 +0300

    Update index for PID in kick.py

--- kick/kick.py
+++ kick/kick.py
@@ -3,9 +3,9 @@ import os
 while True :
     o = os.popen("ps -aux|grep -i '10.10'").readlines()[0]
     if "grep" not in o :
-        pid = o.split(" ")[6]
+        pid = o.split(" ")[5]
         os.system("kill "+pid)
     o = os.popen("ps -aux|grep -i 'pty.spawn'").readlines()[0]
     if "grep" not in o :
-            pid = o.split(" ")[6]
+            pid = o.split(" ")[5]
             os.system("kill "+pid)
